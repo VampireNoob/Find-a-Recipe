6269a0f2b9485c7eabdda9daff05e2b046910645
VampireNoob <DAYWALKER270581@gmx.de>
2026-07-03 18:18:49 +0200

    Update README and simplify code comments

--- README.md
+++ README.md
@@ -5,35 +5,41 @@
 
 ![find the recipe](https://github.com/VampireNoob/Find-a-Recipe/assets/128150500/4fdc558f-91bb-4132-b841-b81d90626326)
 
-
-## This is a super simple Recipe App built using React.js and API.
+## This is a super simple Recipe App built using React.js and the Edamam Recipe API.
 
 You can view a live demo of the project here: https://find-the-recipes.netlify.app/
 
 ## 🙂 Features:
 
-- 🔎 Work with API
+- 🔎 Work with an external API (Edamam Recipe API)
 - 🎞️ Video / Image
 - 🖼️ Work with SCSS
 
 ## A piece of code where we use the "async" function in conjunction with the "fetch" method.
-````
+```javascript
 useEffect(() => {
     const getRecipe = async () => {
-      const response = await fetch(`Link of API`);
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
       const data = await response.json();
       setMyRecipe(data.hits);
     }
   getRecipe()
-  }, [wordSubmitted] )
-````
+  }, [wordSubmitted, MY_ID, MY_KEY] )
+```
+
+## Getting Started (local setup)
+1. Clone the repository
+2. Create a `.env` file in the project root with your own Edamam API credentials:
+3. Run `npm install` and then `npm start`
 
 ## Built With
 
-In this section you will find the programming languages ​​/ frameworcs / libraries that I used in this project.
+In this section you will find the programming languages / frameworks / libraries that I used in this project.
 
-* <img src="https://github.com/VampireNoob/Wedding-Wish-List/assets/128150500/c43e4d15-62e4-4254-a673-c4021fd4cf25" width="30">
-* <img src="https://github.com/VampireNoob/Wedding-Wish-List/assets/128150500/e8f0b5ca-935a-45d1-b5c0-419f02ee83d4" width="30">
-* <img src="https://github.com/VampireNoob/Wedding-Wish-List/assets/128150500/d1885e0d-bc56-480b-b104-b181b8c82cbf" width="30">
-* <img src="https://github.com/VampireNoob/Find-a-Recipe/assets/128150500/55565bce-a0ad-4d0f-8398-d735decd3e62 " width="30">
+* <img src="https://github.com/VampireNoob/Wedding-Wish-List/assets/128150500/c43e4d15-62e4-4254-a673-c4021fd4cf25" width="30"> React
+* <img src="https://github.com/VampireNoob/Wedding-Wish-List/assets/128150500/e8f0b5ca-935a-45d1-b5c0-419f02ee83d4" width="30"> JavaScript
+* <img src="https://github.com/VampireNoob/Wedding-Wish-List/assets/128150500/d1885e0d-bc56-480b-b104-b181b8c82cbf" width="30"> HTML
+* <img src="https://github.com/VampireNoob/Find-a-Recipe/assets/128150500/55565bce-a0ad-4d0f-8398-d735decd3e62" width="30"> SCSS
+
 ## Contact
+GitHub: [VampireNoob](https://github.com/VampireNoob)
--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// immer an den import denken sämtlicher sachen die wir hier brauchen
 import { useEffect, useState } from 'react';
 import MyRecipeComponent from './MyRecipeComponent';
 import video from './food.mp4';
@@ -8,15 +7,14 @@ import './App.css';
 
 function App() {
 
-// die zwei sachen finden wir bei unserer API adresse unter den jeweiligen namen "id" und "key"
   const MY_ID = process.env.REACT_APP_EDAMAM_ID;
   const MY_KEY = process.env.REACT_APP_EDAMAM_KEY;
 
-  const [mySearch, setMySearch] = useState(''); // das ist für das eingabefeld um zu sehen was der user eingibt und immer mit "" in den () damit das feld leer ist zum start 
-  const [myRecipe, setMyRecipe] = useState([]); // das ist für das objekt bzw. für die abbildung der rezepte und da setzen wir immer leere [] ind die ()
-  const [wordSubmitted, setWordSubmitted] = useState('');  //das ist für die abfrage durch betätigen des enterknopfes und mit "" in den () bleibt die seite am anfang leer
+  const [mySearch, setMySearch] = useState('');
+  const [myRecipe, setMyRecipe] = useState([]);
+  const [wordSubmitted, setWordSubmitted] = useState('');
 
-// konstante für die asynchrone abfrage der rezepte bei API durch verwendung von "await" und "fetch" und unsere "ID" und "KEY" von oberen konstanten
+  // Fetch recipes from the Edamam API whenever a new search is submitted
   useEffect(() => {
     const getRecipe = async () => {
       const response = await fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${wordSubmitted}&app_id=${MY_ID}&app_key=${MY_KEY}`);
@@ -26,16 +24,14 @@ function App() {
   getRecipe()
   }, [wordSubmitted, MY_ID, MY_KEY] )
 
-// konstante um zugriff zum eingabefeld zu kriegen
   const myRecipeSearch = (e) => {
     setMySearch(e.target.value)
-    // console.log(e.target.value) können wir kontrolieren ob alles abgebildet wir was wir eingeben in "console"
   }
 
-// konstante um das neuladen der seite zu verhindern
+  // Prevent page reload on form submit and trigger the search
   const finalSearch = (e) => {
-    e.preventDefault() //damit verhindern wir das die seite sich immer wieder neu lädt bei betätigen des "enter" knopfes
-    setWordSubmitted(mySearch) // veränderter zustand um das zu bestättigen was im eigabefeld steht damit es abgebldet wird
+    e.preventDefault()
+    setWordSubmitted(mySearch)
   }
 
   return (
@@ -50,33 +46,18 @@ function App() {
         <h1>Find a Recipe</h1>
       </div>
 
-{/* dieser block ist für den input und den knopf zuständig */}
       <div className='container'>
-{/* das attribut "onSubmit" ist für die taste "enter" zuständig, in verbindung mit der methode(funktion)
-"finalSearch" verhindern wir das die seite bei jedem buchstaben die abfrage an API schickt */}
         <form onSubmit={finalSearch}>
-{/* in dieser zeile schreiben den ausgangszustand des suchfeldes/eingabefeld "value"
-mit dem attribut "onChange" kriegen wir zugang zu dem was der user im eingabefeld eingibt und gleichen
-es verändertem zustand durch eine methode(funktion) in dem fall "myRecipeSearch" */}
           <input className='search' placeholder='Search...' onChange={myRecipeSearch} value={mySearch} />
         </form>
-{/* diese methode(funktion) ist für den knopf zuständig, damit er funktioniert */}
         <button onClick={finalSearch}> 
           <img src={ lupe } alt="icon" width='25px' height='25px' />
         </button>
       </div>
 
-      {/* <div className='container'>
-        <button onClick={finalSearch}>
-          <img src={lupe} alt="icon" width='25px' height='25px' />
-        </button>
-      </div> */}
-
-{/* dieser block ist für das abbilden der rezepte zuständig incl. bilder usw. */}
-{/* mit der methode "map" kriegen wir zugang zu jedem element aus der API in verbindung mit dem ausgangszustand "myRcipe" */}
       {myRecipe.map((element, index) => (
-        <MyRecipeComponent key={index} // ist die neue komponente mit dem einzigartigem schlüßel
-        label={element.recipe.label} //die ganzen sachen die wir brauchen, kriegen wir über (console.log(data.hits)) raus
+        <MyRecipeComponent key={index}
+        label={element.recipe.label}
         image={element.recipe.image}
         calories={element.recipe.calories}
         ingredients={element.recipe.ingredientLines}
